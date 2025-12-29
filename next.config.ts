f42de759ce6f634a6f8eeb8b8642c864b03e8cf6
Nguyễn Gia Hào <giahao9899@gmail.com>
2025-12-29 15:31:14 +0700

    add robots.txt and next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: { ignoreBuildErrors: true },
+  pageExtensions: ["ts", "tsx", "mdx"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
